235606803a0475c09ac67b4575a47d36105da1a8
AMINE-EL-BOUHAMDI <amine.elbouhamdi@gmail.com>
2025-05-18 10:54:41 +0100

    updated action

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY requirements.txt .
  RUN pip install -r requirements.txt
 
-COPY . .
+COPY . . 
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(): 
-   return "Hello from Docker! (EL BOUHAMDI AMINE)" 
+   return "Hello from Docker! (EL BOUHAMDI AMINE) Updated" 
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000) 
