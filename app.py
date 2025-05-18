665114f97e46e480b052037d7ac48be6d3074a78
AMINE-EL-BOUHAMDI <amine.elbouhamdi@gmail.com>
2025-05-18 11:02:10 +0100

    action commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(): 
-   return "Hello from Docker! (EL BOUHAMDI AMINE) Updated 18-05-2025" 
+   return "Hello from Docker! (EL BOUHAMDI AMINE) Updated 18-05-2025 now" 
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000) 
