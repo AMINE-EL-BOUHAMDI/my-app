c4ba8ecc610e4aa2779cc9ca388b344e3562174a
AMINE-EL-BOUHAMDI <amine.elbouhamdi@gmail.com>
2025-05-18 10:57:50 +0100

    updated action 18-05-2025

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(): 
-   return "Hello from Docker! (EL BOUHAMDI AMINE) Updated" 
+   return "Hello from Docker! (EL BOUHAMDI AMINE) Updated 18-05-2025" 
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000) 
